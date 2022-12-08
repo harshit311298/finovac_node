7d40e188fd2486ac67e3a6000f40bab00989b026
no-moin <moinkhan7554@gmail.com>
2022-12-09 00:34:15 +0530

    Code Update .

--- controllers/sliders/controller.js
+++ controllers/sliders/controller.js
@@ -7,5 +7,14 @@ module.exports = {
      } catch (error) {
         
      }
+    },
+    listSliders:async(req,res,next)=>{
+        try {
+            let findSliders=await services.findAllData({status:"ACTIVE"})
+            res.status(200).send({responseCode:200,responseMessage:"Data found successFully.",result:{docs:findSliders}})
+
+        } catch (error) {
+            res.status(501).send({responseCode:501,responseMessage:"Something went wrong ."})
+        }
     }
 }
