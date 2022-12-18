7bd8aa24a378c1dfdc585873856d65ada469998d
no-moin <no-moin@mobiloitte.com>
2022-12-18 21:26:07 +0530

    Code Update email templates .

--- utility/crypto.js
+++ utility/crypto.js
@@ -1,96 +1,110 @@
+require('dotenv');
 const crypto = require('crypto');
 
-var exports = module.exports = {};
+const DATA_ENCRYPTION_ALGORITHM = "aes-256-cbc"
+const DATA_ENCRYPTION_KEY       = "d0ct0rd3nTisT"
+const DATA_ENCRYPTION_IV        = "1e1511c59e6479a0"
+const DATA_ENCRYPTION_IV_32     = "1e1511c59e6479a00a9746e95c1151e1"
 
-const ALGORITHM = {
+const HASH_PEPPER      = "ajg3bEdNaWpuZQ"
+const HASH_SALT_ROUNDS = 10
+const HASH_ENTROPY     = 5
+function CryptoUtil() {}
+
+CryptoUtil.prototype.encrypt = function(text) {
+  if(!text) return '';
+
+  const ENCRYPTION_ALGO = DATA_ENCRYPTION_ALGORITHM;
+  const ENCRYPTION_KEY  = crypto.createHash('sha256').update(String(DATA_ENCRYPTION_KEY)).digest('base64').substr(0, 32);
+
+  const iv      = Buffer.alloc(16, DATA_ENCRYPTION_IV_32);
+  const cipher  = crypto.createCipheriv(ENCRYPTION_ALGO, ENCRYPTION_KEY, iv);
+  let encrypted = cipher.update(text);
+  encrypted     = Buffer.concat([encrypted, cipher.final()]);
+  
+  return iv.toString('hex') + ':' + encrypted.toString('hex');
+}
+
+CryptoUtil.prototype.decrypt = function(text) {
+  if(!text) return '';
+  
+  const ENCRYPTION_ALGO = DATA_ENCRYPTION_ALGORITHM;
+  const ENCRYPTION_KEY  = crypto.createHash('sha256').update(String(DATA_ENCRYPTION_KEY)).digest('base64').substr(0, 32);
+
+  const textParts     = text.split(':');
+  const iv            = Buffer.from(textParts.shift(), 'hex');
+  const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+  const decipher      = crypto.createDecipheriv(ENCRYPTION_ALGO, ENCRYPTION_KEY, iv);
+  let decrypted       = decipher.update(encryptedText);
+  decrypted           = Buffer.concat([decrypted, decipher.final()]);
+  
+  return decrypted.toString();
+}
+
+function randomizeStrings(a, b, c = 0) {
+  if(c === (+HASH_ENTROPY || 5)) return [...a, ...b].join('');
+
+  a = String(a);
+  b = String(b);
+
+  var d = [...a, ...b];
+  var l = d.filter((_,i) => 0 === i%2);
+  var r = d.filter((_,i) => 0 !== i%2);
+
+  return randomizeStrings(l.join(''), r.join(''), c+1);
+}
+
+CryptoUtil.prototype.hash = function (text) {
+  if(!text) return '';
+  
+  const saltRnd = +HASH_SALT_ROUNDS;
+  const pepper  = HASH_PEPPER;
+  const subject = randomizeStrings(text, pepper);
+
+  const salt = bcrypt.genSaltSync(saltRnd);
+  
+  return bcrypt.hashSync(subject, salt);
+}
+
+CryptoUtil.prototype.hashCompare = function (text, hash) {
+  if(!text) return false;
+  if(!hash) return false;
+
+  const pepper  = HASH_PEPPER;
+  const subject = randomizeStrings(text, pepper);
+  
+  return bcrypt.compareSync(subject, hash);
+}
+
+CryptoUtil.prototype.mask = function(text='', from=4, to=10, maskWith='*') {
+  const maskPattern = maskWith.repeat(to-from);
+  return text.replace(text.substring(from, to), maskPattern);
+}
+
+CryptoUtil.prototype.public = {
+  encrypt: function(text, pathToPublicKey = PKE_PATH_TO_PUBLIC_KEY) {
+    const absolutePath = path.resolve(pathToPublicKey)
+    const publicKey    = fs.readFileSync(absolutePath, 'utf8')
+    const buffer       = Buffer.from(text, 'utf8')
+    const encrypted    = crypto.publicEncrypt(publicKey, buffer)
     
-    /**
-     * GCM is an authenticated encryption mode that
-     * not only provides confidentiality but also 
-     * provides integrity in a secured way
-     * */  
-    "BLOCK_CIPHER": 'aes-256-gcm',
+    return encrypted.toString('base64');
+  },
 
-    /**
-     * 128 bit auth tag is recommended for GCM
-     */
-    "AUTH_TAG_BYTE_LEN": 16,
-
-    /**
-     * NIST recommends 96 bits or 12 bytes IV for GCM
-     * to promote interoperability, efficiency, and
-     * simplicity of design
-     */
-    "IV_BYTE_LEN": 12,
-
-    /**
-     * Note: 256 (in algorithm name) is key size. 
-     * Block size for AES is always 128
-     */
-    "KEY_BYTE_LEN": 32,
-
-    /**
-     * To prevent rainbow table attacks
-     * */
-    "SALT_BYTE_LEN": 16
+  decrypt: function(text, pathToPrivateKey = PKE_PATH_TO_PRIVATE_KEY) {
+    const absolutePath = path.resolve(pathToPrivateKey)
+    const privateKey   = fs.readFileSync(absolutePath, 'utf8')
+    const buffer       = Buffer.from(text, 'base64')
+    const decrypted    = crypto.privateDecrypt(
+      {
+        key: privateKey.toString(),
+        passphrase: PKE_PASS_PHRASE,
+      },
+      buffer
+    );
+    
+    return decrypted.toString('utf8')
+  }
 }
 
-const getIV = () => crypto.randomBytes(ALGORITHM.IV_BYTE_LEN);
-exports.getRandomKey = getRandomKey = () => crypto.randomBytes(ALGORITHM.KEY_BYTE_LEN);
-
-/**
- * To prevent rainbow table attacks
- * */
-exports.getSalt = getSalt = () => crypto.randomBytes(ALGORITHM.SALT_BYTE_LEN);
-
-/**
- * 
- * @param {Buffer} password - The password to be used for generating key
- * 
- * To be used when key needs to be generated based on password.
- * The caller of this function has the responsibility to clear 
- * the Buffer after the key generation to prevent the password 
- * from lingering in the memory
- */
-exports.getKeyFromPassword = getKeyFromPassword = (password, salt) => {
-    return crypto.scryptSync(password, salt, ALGORITHM.KEY_BYTE_LEN);
-}
-
-/**
- * 
- * @param {Buffer} messagetext - The clear text message to be encrypted
- * 
- * The caller of this function has the responsibility to clear 
- * the Buffer after the encryption to prevent the message text 
- * and the key from lingering in the memory
- */
-exports.encrypt = encrypt = (messagetext) => {
-    const iv = getIV();
-    const cipher = crypto.createCipheriv(
-        ALGORITHM.BLOCK_CIPHER, process.env.DATA_ENCRYPTION, iv,
-        { 'authTagLength': ALGORITHM.AUTH_TAG_BYTE_LEN });
-    let encryptedMessage = cipher.update(messagetext);
-    encryptedMessage = Buffer.concat([encryptedMessage, cipher.final()]);
-    return Buffer.concat([iv, encryptedMessage, cipher.getAuthTag()]);
-}
-
-/**
- * 
- * @param {Buffer} ciphertext - Cipher text
- * 
- * The caller of this function has the responsibility to clear 
- * the Buffer after the decryption to prevent the message text 
- * and the key from lingering in the memory
- */
-exports.decrypt = decrypt = (ciphertext) => {
-    const authTag = ciphertext.slice(-16);
-    const iv = ciphertext.slice(0, 12);
-    const encryptedMessage = ciphertext.slice(12, -16);
-    const decipher = crypto.createDecipheriv(
-        ALGORITHM.BLOCK_CIPHER, process.env.DATA_ENCRYPTION, iv,
-        { 'authTagLength': ALGORITHM.AUTH_TAG_BYTE_LEN });
-    decipher.setAuthTag(authTag);
-    let messagetext = decipher.update(encryptedMessage);
-    messagetext = Buffer.concat([messagetext, decipher.final()]);
-    return messagetext;
-}
+module.exports = new CryptoUtil();
